4b1e2bc7462ca9822571b7e34e3d466d2d8f1f69
David <41311088+biagiola@users.noreply.github.com>
2019-12-13 16:13:51 -0300

    Add files via upload

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,24 +1,16 @@
-import React, { createContext, useState } from 'react';
-import uuid from 'uuid/v1';
+import React, { createContext, useReducer } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([
+  const [books, dispatch] = useReducer(bookReducer, [
     { title: 'name of the wind', id: 1 },
     { title: 'the way of kings', id: 2 },
   ]);
-  
-  const addBook = (title, author) => {
-    setBooks([...books, {title, author, id: uuid()}])
-  };
-  
-  const removeBook = (id) => {
-    setBooks(books.filter( book => book.id !== id ))
-  };
 
   return (
-    <BookContext.Provider value={{books, addBook, removeBook}}>
+    <BookContext.Provider value={{books, dispatch}}>
       { props.children }
     </BookContext.Provider>
   )
